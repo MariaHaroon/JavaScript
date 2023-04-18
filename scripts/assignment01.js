6f2769ed92b3bd8e868c4d6bbeb35c67c9051849
Maria Haroon <mejoramiga.parasiempre@gmail.com>
2023-04-18 17:13:47 +0500

    tasks update

--- scripts/assignment01.js
+++ scripts/assignment01.js
@@ -8,7 +8,87 @@
 // message to display the calculated zakat value. The message should look like this:
 // "Your zakat value is xxx"
 
-var zakatPercentage = 0.025
-var userInput =+ prompt("Mention your asset value")
-var result = zakatPercentage * userInput
-alert ("Your calculated Zakat is  "+ result )
+// var zakatPercentage = 0.025
+// var userInput =+ prompt("Mention your asset value")
+// var result = zakatPercentage * userInput
+// alert ("Your calculated Zakat is  "+ result )
+// console.log(alert)
+
+// Task 2:
+// calculate the fitrah amount, first, ask the user to enter the total number of family
+// members using the prompt and store the value in a variable called
+// "familyMembers". Next, ask the user to choose a fitrah method by providing them
+// options using the prompt, and store the selected method and its price in variables.
+// Then, use an if-else block to check the user's input and calculate the fitrah amount
+// by multiplying the selected method's price with the number of family members.
+// Finally, display the calculated fitrah amount using an alert message.
+
+// var familyMembers =+ prompt ("Enter the total number of family members:")
+// alert ("PLEASE READ THIS: \n 2 kg wheet costs Rs 250 \n 3.5 kg barley costs Rs 450 \n 3.5 kg date costs Rs 2100 \n 3.5 kg raisin costs Rs 2800")
+// var userInput = prompt("Choose your Fitrah method below: \n 1- WHEET (2 KG)\n 2- BARLEY (3.5 KG)\n 3- DATE (3.5 KG)\n 4- RAISIN (3.5 KG)", " TYPE LETTER A-WHEET \n B-BARLEY \n C-DATE \n D-RAISIN")
+// var A =  250;
+// var B =  450;
+// var C =  2100;
+// var D =  2800;
+//  if (userInput == 'A') {
+//     var result = 250 * familyMembers;
+// } else if(userInput == 'B') {
+//      result = 450 * familyMembers;
+// } else if (userInput == 'C') {
+//      result = 2100 * familyMembers;
+// } else if (userInput == 'D') {
+//      result = 2800 * familyMembers;
+// }
+// alert ("Your Fitrah Amount is  "+ result )
+// console.log(alert)
+
+// Task 3:
+// Create a program that generates a random number between 1 and 10 and stores it
+// in a variable called "secretNumber". Then, ask the user to enter a guess for the
+// secret number using a prompt.
+// Use an if-else statement to check if the user's guess matches the secret number. If
+// the guess is correct, display a message using an alert saying "Congratulations! You
+// guessed the secret number". Otherwise, if the guess is too high or too low, display
+// an appropriate message informing the user to guess again.
+
+// var secretNumber 
+// const guess =+ prompt('Guess the number between 1 and 10');
+// if (guess == secretNumber)
+//   console.log('Congratulations! You guessed the secret number');
+//  else
+//   console.log(' '+);
+
+// Task 4:
+// Create a program that asks the user to enter a name, and then prints out the name
+// with the first letter capitalized (Make your name in capitalized case).
+
+// var inputname = prompt("Enter a name: ")
+// var firstLetter = inputname.slice(0,1);
+// firstLetter = inputname.toUpperCase();
+
+// var remainingLetters = inputname.slice()
+// var remainingLetters = inputname.toLowerCase();
+// var Titlecase = firstLetter +  remainingLetters;
+// alert ("User is "+ Titlecase )
+
+
+// Task 5:
+// In this task, you will be creating two empty arrays called "contactNumbers" and
+// "contactNames". Using the prompt, you will ask the user to enter a contact number
+// and contact name. You will then push these values into their respective arrays
+// using the push method. After adding all the contacts, you will display the contact
+// numbers and names in the console. To do this, you will need to use a for loop to
+// iterate through both arrays and log each element to the console.
+// Make sure to use descriptive variable names and comment on your code for clarity.
+
+// contactNumbers
+// contactNames
+
+
+
+
+// Task 6:
+// Create an Array that contains different products, and get input from the user in which you
+// ask your user to give the position of that element he/she wants. Now remove that Item
+// from your array and console the removed item, Also display the remaining items in the
+// array and total number of items remaining.
